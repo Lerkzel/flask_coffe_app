8bd1d7b3c6767ea50c378e37e9321e40764f0eb7
Lerkzel <leraaa.zelenko@gmail.com>
2025-03-27 18:06:59 +0200

    Update app.py with basic Flask setup

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-flask_coffee_app/  
-app.py  
-database.py  
-visualization.py  
-requirements.txt  
-templates/  
-index.html  
-visualization.html  
-static/  
-styles.css
+from flask import Flask, render_template
+from database import get_top_10_countries
+from visualization import generate_plots
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/visualization")
+def visualization():
+    generate_plots()
+    return render_template("visualization.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
